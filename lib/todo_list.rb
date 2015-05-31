24f2b78b58877896fb54600b78bc0fd89b05ec84
John Daugherty <johndaugherty1986@gmail.com>
2015-05-31 17:54:52 -0400

    added modify and remove methods to menu. fixed typos

--- lib/todo_list.rb
+++ lib/todo_list.rb
@@ -1,25 +1,29 @@
 require_relative "../db/setup.rb"
 require_relative "todo"
-
 class TodoList
 
   def start
     loop do
       @todos = Todo.all
       system('clear')
-      puts "----Todo List----"
-
+      puts "
+      +--------------------------------------------------------------------+
+      |                             TODO LIST                              |
+      +--------------------------------------------------------------------+"
       view_todos
 
       puts
       puts "What would you like to do?"
-      puts "1) Exit 2) Add Todo 3) Mark Todo As Complete"
+      puts
+      puts "1) Exit 2) Add Todo 3) Mark Todo As Complete 4) Remove Todo 5) Edit Todo"
       print " > "
       action = gets.chomp.to_i
       case action
       when 1 then exit
       when 2 then add_todo
       when 3 then mark_todo
+      when 4 then destroy_todo
+      when 5 then edit_todo
       else
         puts "\a"
         puts "Not a valid choice"
@@ -28,21 +32,35 @@ class TodoList
   end
 
   def add_todo
-    puts "What do you need to do?"
+    puts "What would you like to add to your todo list?"
     Todo.create(entry: get_input)
   end
 
   def mark_todo
-    puts "what todo have you done?"
-    # get_input
+    puts "which todo have you done?
+      (please pick id number)"
     Todo.update(get_input, completed: true)
   end
 
   def view_todos
     @todos.each do |item|
-      puts "#{item.id} #{item.entry} #{item.completed}"
-
+      puts
+      puts "#{item.id} | #{item.entry} | #{item.completed}"
     end
+  end
+
+  def destroy_todo
+    puts "which todo would you like to remove?
+      (please pick id number)"
+    Todo.destroy(get_input)
+  end
+
+ def edit_todo
+    puts
+    "which todo would you like to edit? (please pick id number)"
+      edit_todo = get_input
+    puts "what would you like it to say now?"
+    Todo.update(edit_todo, entry: get_input)
   end
 
   private
